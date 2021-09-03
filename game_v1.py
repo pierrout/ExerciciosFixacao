711f577b2021774c4fbc3046dcba712b72cf54f8
Ana Flávia <pierrout77@gmail.com>
2021-09-03 19:11:16 -0300

    Update game_v1.py

--- game_v1.py
+++ game_v1.py
@@ -2,32 +2,56 @@
 level = int(input("Qual nível você gostaria de jogar? 1/2/3/4/5 \n"))
 length = 500/level
 
-
+# atribuindo a variavel root uma tupla tk
 root = Tk()
+# acessando o titulo do objeto root e dando o titulo
 root.title("Ping Pong")
+# resizable é um método de root
+# resizable permite aumentar o tamanho da tela que o tKinter proporciona
 root.resizable(0,0)
 root.wm_attributes("-topmost", -1)
 
+# criação de um canvas
 canvas = Canvas(root, width=800, height=600, bd=0,highlightthickness=0)
+# pack posiciona o widget
+# é a forma mais simples de place() e grid ()
 canvas.pack()
 
+# deve ta recarregando o root com o titulo e etc
 root.update()
 
 # Variável
 count = 0
 lost = False
 
+# Se a classe é a forma e o objeto é o que vai ser encaixado
+# então deve esta organizando a estrutura 
 class Bola:
+    # def é a declaração de uma função
+    # os argumentos estão entre parentes
+    # mas o que é retornado ? 
+    # __init__ é o construtor - sempre que um novo obj for instanciado o que ta dentro de Bola será executado
+    
     def __init__(self, canvas, Barra, color):
         self.canvas = canvas
         self.Barra = Barra
         self.id = canvas.create_oval(0, 0, 15, 15, fill=color)
         self.canvas.move(self.id, 245, 200)
 
+        # starts_x é uma lista
         starts_x = [-3, -2, -1, 1, 2, 3]
+        # escolhe aleatoriamente os numeros dentro de starts_x
+        # shuffle reorganiza a ordem dos intems
+        # então depois de ter posto os elementos em ordem crescente, escolhe "aleatoriamente"?
         random.shuffle(starts_x)
-
+        
+        # self REPRESENTA a instancia de uma classe 
+        # self aponta para o objeto, faz referencia ao objeto
+        # instanciei a classe x e atribui o atributo de indice 0 da lista
+        # com self consigo acessar os atributos e métodos de uma classe 
         self.x = starts_x[0]
+        
+        # acessando um atributo de y 
         self.y = -3
 
         self.canvas_height = self.canvas.winfo_height()
@@ -107,7 +131,7 @@ class Barra:
         if self.pos[2] <= self.canvas_width:
             self.x = 3
 
-
+# inicializou sem o self ?
 def start_game(event):
     global lost, count
     lost = False
